f9b43abfa5e8d9bd4663e72af11d7d9f77265d18
david lewis and roger yee <roger@>
2018-01-09 10:02:20 -0800

    add a new font for header 2, formatting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $(".clickable1").click(function() {
-    $(".ghost1").toggle();
-    $("#secondGhost").toggle();
+    $(".ghost1").fadeToggle();
+    $("#secondGhost").fadeToggle();
   });
   $(".clickable2").click(function() {
-    $(".ghost2").toggle();
+    $(".ghost2").slideToggle();
   });
 });
